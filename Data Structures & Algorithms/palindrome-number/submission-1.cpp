d83b267c32f8cd81d45a71ad9c37243e96d8b939
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-06-12 14:56:48 +0530

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isPalindrome(int x) {
+        long long ans = 0;
+        int num = x;
+        int rem;
+        if (x < 0) {
+            return false;
+        }
+
+        while (x) {
+            rem = x % 10;
+            if (ans > INT_MAX/10 || ans < INT_MIN/10){
+                return false;
+            }
+            ans = ans * 10 + rem;
+            x /= 10;
+        }
+        return ans == num;
+    }
+};
