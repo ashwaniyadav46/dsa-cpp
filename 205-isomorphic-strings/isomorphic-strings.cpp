fd97244d949eea6c30ed588f93548404d8d8686b
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-08-02 17:17:33 +0530

    Time: 4 ms (22.91%) | Memory: 9.3 MB (58.61%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,6 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> mp1;
         unordered_map<char, char> mp2;
+        if(s.size() != t.size()) return false;
 
         for (int i = 0; i < s.size(); i++) {
 
