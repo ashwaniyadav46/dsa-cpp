a90d017e57ab74694af4378d82ac17924d7f4415
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-09-04 21:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (59.31%) - LeetSync

--- DSA/problem/498-diagonal-traverse/diagonal-traverse.cpp
+++ DSA/problem/498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,21 +3,50 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>> diagonal(m + n - 1);
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                diagonal[i + j].push_back(mat[i][j]);
-            }
-        }
+
         vector<int> ans;
-        for(int d = 0; d < m + n - 1; d++){
-            if(d % 2 == 0){
-                reverse(diagonal[d].begin(), diagonal[d].end());
+        int row = 0;
+        int col = 0;
+
+        bool up = true;
+
+        while (ans.size() < m * n) {
+
+            ans.push_back(mat[row][col]);
+
+            if (up) {
+
+                if (col == n - 1) {
+                    row++;
+                    up = false;
+                }
+                else if (row == 0) {
+                    col++;
+                    up = false;
+                }
+                else {
+                    row--;
+                    col++;
+                }
+
             }
-            for(int num : diagonal[d]){
-                ans.push_back(num);
+            else {
+
+                if (row == m - 1) {
+                    col++;
+                    up = true;
+                }
+                else if (col == 0) {
+                    row++;
+                    up = true;
+                }
+                else {
+                    row++;
+                    col--;
+                }
             }
         }
+
         return ans;
     }
 };
