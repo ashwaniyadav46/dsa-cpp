2cfaffc1f0b9341a1dfd22b0766b82df2cdc2c68
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-09-21 17:25:18 +0530

    Time: 2 ms (2.66%) | Memory: 8.3 MB (53.50%) - LeetSync

--- DSA/problem/742-to-lower-case/to-lower-case.cpp
+++ DSA/problem/742-to-lower-case/to-lower-case.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(char &c : s){
-            c = tolower(c);
+        for(char &c :s){
+            if(c >= 'A' && c <='Z'){
+                c += 32;
+            }
         }
         return s;
     }
