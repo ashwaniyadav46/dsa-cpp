1614f41380132ecddb6d5db2d0e5c7ca544848fe
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-09-22 17:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (71.17%) - LeetSync

--- DSA/problem/387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ DSA/problem/387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> mp;
-        for (char& c : s) {
-            mp[c]++;
+        int freq[26] = {0};
+        for (char c : s){
+            freq[c - 'a']++;
+
         }
-        for (int i = 0; i < s.length(); i++) {
-            if (mp[s[i]] == 1) {
+        for(int i = 0; i < s.length(); i++){
+            if(freq[s[i] - 'a'] == 1){
                 return i;
             }
         }
-    
-
         return -1;
     }
 };
