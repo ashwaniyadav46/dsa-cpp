87767dba5a70dbc2b422eba68bae15e0006648c5
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-09-23 17:21:05 +0530

    Time: 2 ms (15.95%) | Memory: 8.8 MB (17.11%) - LeetSync

---
+++ DSA/problem/2235-capitalize-the-title/capitalize-the-title.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        stringstream ss(title);
+        string word, result ="";
+        while(ss >> word){
+            for(char &c : word){
+                c = tolower(c);
+            }
+            if(word.length() > 2){
+                word[0] = toupper(word[0]);
+            }
+            if(!result.empty()){
+                result += " ";
+            }
+            result += word;
+        }
+        return result;
+    }
+};
