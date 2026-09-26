3fd7fc2f07a2e9e534f2f7dcf0323bc20dda53df
Ashwani Yadav <yadavashwani4651@gmail.com>
2026-09-26 22:25:04 +0530

    Time: 12 ms (37.77%) | Memory: 19.2 MB (91.34%) - LeetSync

---
+++ DSA/problem/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int freq[26] = {0};
+        for(char c : s){
+            freq[c - 'a']++;
+        }
+        for(char c : t){
+            freq[c - 'a']--;
+        }
+        int steps = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > 0){
+                steps += freq[i]; 
+            }
+        }
+        return steps;
+    }
+};
